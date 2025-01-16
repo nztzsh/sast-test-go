37edc7d714d60fe662ca6ed6edf48990b7e8dd5d
Nitesh Saxena <nsaxena@endor.ai>
2025-01-16 20:26:51 +0530

    remove const

--- handlers/proxy.go
+++ handlers/proxy.go
@@ -5,12 +5,10 @@ import (
 	"net/http"
 )
 
-const (
-	_proxyBaseURL = "https://google"
-)
-
 // Proxy is a handler that forwards requests to the Google search engine.
 func Proxy(w http.ResponseWriter, r *http.Request) {
+
+	_proxyBaseURL := "https://google"
 
 	url := fmt.Sprintf(
 		"%s.%s/search?q=%s",
